ef7dc15c7b572a55d01c3b29cc436c5c748ead43
Shineaki <halmosadam92@gmail.com>
2024-12-08 15:42:06 +0100

    Init

--- rogue/main.py
+++ rogue/main.py
@@ -1,83 +1,42 @@
-from enum import Enum
 from os.path import join
-from typing import Optional
 
 import pygame
 
-
-class AnimState(Enum):
-    IDLE = 1
-    RUN = 2
-    HIT = 3
+from rogue.player import Player
 
 
-class Direction(Enum):
-    LEFT = 1
-    RIGHT = 2
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, groups: Optional[pygame.sprite.Group] = None):
-        super().__init__(groups)
-        self.animation_length = 4
+class TileMap:
+    def __init__(self, size: tuple[int]):
         self.images = {
-            AnimState.IDLE: [pygame.image.load(
-                join("resources", "character", "elf_m", "idle", f"elf_m_idle_anim_f{i}.png")).convert_alpha() for i in range(self.animation_length)],
-            AnimState.RUN: [pygame.image.load(
-                join("resources", "character", "elf_m", "run", f"elf_m_run_anim_f{i}.png")).convert_alpha() for i in range(self.animation_length)]
+            "wall": pygame.image.load(join("resources", "tiles", "wall_mid.png")).convert_alpha(),
+            "floor": pygame.image.load(join("resources", "tiles", "floor_1.png")).convert_alpha()
         }
-        self.animation_idx = 0
-        self.current_animation_state = AnimState.IDLE
-        self.image = self.images[self.current_animation_state][self.animation_idx]
-        self.animation_timer = 0
-        self.rect = pygame.Rect(0, 0, 32, 32)
-        self.direction = pygame.Vector2()
-        self.speed = 100
-        self.current_direction = Direction.RIGHT
-        self.position = pygame.Vector2(16, 16)
-
-    def update(self, dt: float):
-        self.animation_timer += 1
-        if self.animation_timer > 5:
-            self.animation_timer = 0
-            self.animation_idx = (self.animation_idx +
-                                  1) % self.animation_length
-            self.image = self.images[self.current_animation_state][self.animation_idx]
-
-        keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
-        self.direction = self.direction.normalize() if self.direction else self.direction
-
-        if self.direction.length() > 0.0:
-            self.current_animation_state = AnimState.RUN
-        else:
-            self.current_animation_state = AnimState.IDLE
-        if self.direction.x > 0:
-            self.current_direction = Direction.RIGHT
-        if self.direction.x < 0:
-            self.current_direction = Direction.LEFT
-
-        if self.current_direction == Direction.LEFT:
-            self.image = pygame.transform.flip(
-                self.images[self.current_animation_state][self.animation_idx], True, False)
-        self.position += self.direction * self.speed * dt
-        self.rect.center = self.position
+        self.sprites = []
+        self.tiles_group = pygame.sprite.Group()
+        for x in range(size[0]):
+            for y in range(size[1]):
+                c_sprite = pygame.sprite.Sprite(self.tiles_group)
+                if y == 0 or y == size[1]-1:
+                    c_sprite.image = self.images["wall"]
+                else:
+                    c_sprite.image = self.images["floor"]
+                c_sprite.rect = pygame.rect.Rect(x*16, 20+y*16, 16, 16)
 
 
 class Game:
     def __init__(self):
         pygame.init()
+        self.screen_size = pygame.Vector2(640, 360)
+        self.tilemap_size = (16, 16)
+        self.screen_tile_size = (40, 20)
+
         self.screen = pygame.display.set_mode(
             (640, 360), flags=pygame.SCALED, vsync=0)
         self.clock = pygame.time.Clock()
         self.running = True
         self.dt = 0
 
-        self.player_pos = pygame.Vector2(
-            self.screen.get_width() / 2,
-            self.screen.get_height() / 2
-        )
+        self.tilemap = TileMap(self.screen_tile_size)
 
         self.player_group = pygame.sprite.Group()
         self.object = Player(self.player_group)
@@ -94,6 +53,7 @@ class Game:
             self.screen.fill("black")
 
             self.player_group.update(self.dt)
+            self.tilemap.tiles_group.draw(self.screen)
             self.player_group.draw(self.screen)
 
             pygame.display.flip()
---
+++ rogue/player.py
@@ -0,0 +1,66 @@
+
+from enum import Enum
+from os.path import join
+from typing import Optional
+
+import pygame
+
+
+class AnimState(Enum):
+    IDLE = 1
+    RUN = 2
+    HIT = 3
+
+
+class Direction(Enum):
+    LEFT = 1
+    RIGHT = 2
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups: Optional[pygame.sprite.Group] = None):
+        super().__init__(groups)
+        self.animation_length = 4
+        self.images = {
+            AnimState.IDLE: [pygame.image.load(
+                join("resources", "character", "elf_m", "idle", f"elf_m_idle_anim_f{i}.png")).convert_alpha() for i in range(self.animation_length)],
+            AnimState.RUN: [pygame.image.load(
+                join("resources", "character", "elf_m", "run", f"elf_m_run_anim_f{i}.png")).convert_alpha() for i in range(self.animation_length)]
+        }
+        self.animation_idx = 0
+        self.current_animation_state = AnimState.IDLE
+        self.image = self.images[self.current_animation_state][self.animation_idx]
+        self.animation_timer = 0
+        self.rect = pygame.Rect(0, 0, 32, 32)
+        self.direction = pygame.Vector2()
+        self.speed = 100
+        self.current_direction = Direction.RIGHT
+        self.position = pygame.Vector2(16, 16)
+
+    def update(self, dt: float):
+        self.animation_timer += 1
+        if self.animation_timer > 5:
+            self.animation_timer = 0
+            self.animation_idx = (self.animation_idx +
+                                  1) % self.animation_length
+            self.image = self.images[self.current_animation_state][self.animation_idx]
+
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+
+        if self.direction.length() > 0.0:
+            self.current_animation_state = AnimState.RUN
+        else:
+            self.current_animation_state = AnimState.IDLE
+        if self.direction.x > 0:
+            self.current_direction = Direction.RIGHT
+        if self.direction.x < 0:
+            self.current_direction = Direction.LEFT
+
+        if self.current_direction == Direction.LEFT:
+            self.image = pygame.transform.flip(
+                self.images[self.current_animation_state][self.animation_idx], True, False)
+        self.position += self.direction * self.speed * dt
+        self.rect.center = self.position
