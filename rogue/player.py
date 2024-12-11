5bcde1b1d0f20f21c9ed7dd146efc4fc94f52f8f
Shineaki <halmosadam92@gmail.com>
2024-12-12 00:18:33 +0100

    :poop: Grid movement - to refactor

--- rogue/player.py
+++ rogue/player.py
@@ -13,8 +13,11 @@ class AnimState(Enum):
 
 
 class Direction(Enum):
+    NULL = 0
     LEFT = 1
     RIGHT = 2
+    UP = 3
+    DOWN = 4
 
 
 class Player(pygame.sprite.Sprite):
@@ -34,9 +37,20 @@ class Player(pygame.sprite.Sprite):
         self.rect = pygame.rect.Rect(starting_pos, (16, 16))
 
         self.direction = pygame.Vector2()
-        self.speed = 100
+        self.speed = 50
         self.current_direction = Direction.RIGHT
         self.position = pygame.Vector2(starting_pos[0], starting_pos[1])
+
+        self.moving = False
+        self.moving_direction = Direction.NULL
+        self.target_position = None
+
+        self.dir_to_pos = {
+            Direction.UP: (0, -16),
+            Direction.RIGHT: (16, 0),
+            Direction.DOWN: (0, 16),
+            Direction.LEFT: (-16, 0),
+        }
 
     def update(self, dt: float):
         self.animation_timer += 1
@@ -46,23 +60,62 @@ class Player(pygame.sprite.Sprite):
                                   1) % self.animation_length
             self.image = self.images[self.current_animation_state][self.animation_idx]
 
-        keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
-        self.direction = self.direction.normalize() if self.direction else self.direction
-
-        if self.direction.length() > 0.0:
-            self.current_animation_state = AnimState.RUN
-        else:
+        if not self.moving:
             self.current_animation_state = AnimState.IDLE
-        if self.direction.x > 0:
-            self.current_direction = Direction.RIGHT
-        if self.direction.x < 0:
-            self.current_direction = Direction.LEFT
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_d]:
+                self.moving_direction = Direction.RIGHT
+            if keys[pygame.K_a]:
+                self.moving_direction = Direction.LEFT
+            if keys[pygame.K_s]:
+                self.moving_direction = Direction.DOWN
+            if keys[pygame.K_w]:
+                self.moving_direction = Direction.UP
+            if self.moving_direction != Direction.NULL:
+                self.moving = True
+                self.target_position = pygame.Vector2(
+                    self.rect.centerx + self.dir_to_pos[self.moving_direction][0], self.rect.centery + self.dir_to_pos[self.moving_direction][1])
+
+        if self.moving:
+            self.current_animation_state = AnimState.RUN
+            self.direction = self.rect.center - self.target_position
+            print(self.direction)
+            if self.direction != [0, 0]:
+                self.direction = self.direction.normalize() if self.direction else self.direction
+                self.position -= self.direction * self.speed * dt
+                self.rect.x = self.position.x
+                self.rect.y = self.position.y
+            else:
+                self.moving = False
+                self.target_position = None
+                self.moving_direction = Direction.NULL
+
+        if self.moving_direction in [Direction.LEFT, Direction.RIGHT]:
+            self.current_direction = self.moving_direction
 
         if self.current_direction == Direction.LEFT:
             self.image = pygame.transform.flip(
                 self.images[self.current_animation_state][self.animation_idx], True, False)
-        self.position += self.direction * self.speed * dt
-        self.rect.x = self.position.x
-        self.rect.y = self.position.y
+
+        # self.target_position != self.rect.center
+
+        # keys = pygame.key.get_pressed()
+        # self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        # self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        # self.direction = self.direction.normalize() if self.direction else self.direction
+
+        # if self.direction.length() > 0.0:
+        #     self.current_animation_state = AnimState.RUN
+        # else:
+        #     self.current_animation_state = AnimState.IDLE
+        # if self.direction.x > 0:
+        #     self.current_direction = Direction.RIGHT
+        # if self.direction.x < 0:
+        #     self.current_direction = Direction.LEFT
+
+        # if self.current_direction == Direction.LEFT:
+        #     self.image = pygame.transform.flip(
+        #         self.images[self.current_animation_state][self.animation_idx], True, False)
+        # self.position += self.direction * self.speed * dt
+        # self.rect.x = self.position.x
+        # self.rect.y = self.position.y
